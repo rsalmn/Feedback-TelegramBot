97c09433f726e3895fb3083b0b13b092e5359a29
GarudaProjects <80512930+GarudaProjects@users.noreply.github.com>
2021-12-21 11:50:50 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+TG_TOKEN = "1625666269:AAFo3l_wF2Lclg-4DGfDZUVprTBGZx-OigI"
+PROXY = None
+CHAT_ID = -1001445044781 # The chat group or individual chat id of forwarded message destination
+BOT_TIMEOUT = 10
+BOT_RETRIES = 5
+USER_JSON = 'user_book.json'
+ADMIN_LIST = 'admin.txt'
